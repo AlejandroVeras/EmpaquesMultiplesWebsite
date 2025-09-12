a68f28dba990f5a6cdebd752a2c2a7951fb35092
AlejandroVeras <verasetanislao21@gmail.com>
2025-09-11 20:58:03 -0400

    Update supabase.js

--- lunch-system/src/lib/supabase.js
+++ lunch-system/src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://dqebwnokccpdowakkmec.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZWJ3bm9rY2NwZG93YWtrbWVjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc2MzcxMjcsImV4cCI6MjA3MzIxMzEyN30.RT-W28-jBLvRgzCoc8fpJAs0Fk4JoKoQNmk1Naqvi48'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
