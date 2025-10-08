139ff5a5ba8c64fed2a5bffc0c0d73f9f89f1ce5
AlejandroVeras <verasetanislao21@gmail.com>
2025-10-08 13:52:37 -0600

    Update firebase-config.js

--- almuerzo/js/firebase-config.js
+++ almuerzo/js/firebase-config.js
@@ -1,11 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyDcwkUmqaiguV2MoEPsz8HSWUD9uzTIS-w",
   authDomain: "empaques-multiples-almuerzo.firebaseapp.com",
@@ -17,9 +10,9 @@ const firebaseConfig = {
   measurementId: "G-2NRREN25E3"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inicializar Firebase usando la API de compatibilidad
+firebase.initializeApp(firebaseConfig);
+
 // Referencias a servicios de Firebase
 const auth = firebase.auth();
 const database = firebase.database();
