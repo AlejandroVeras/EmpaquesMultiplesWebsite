53205bda38da7d405cd100fb4f5519281de424ef
AlejandroVeras <verasetanislao21@gmail.com>
2025-10-08 13:40:35 -0600

    Update firebase-config.js

--- almuerzo/js/firebase-config.js
+++ almuerzo/js/firebase-config.js
@@ -1,21 +1,28 @@
-// Firebase Configuration
-// IMPORTANTE: Reemplazar estos valores con las credenciales reales de Firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_AUTH_DOMAIN",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_STORAGE_BUCKET",
-    messagingSenderId: "TU_MESSAGING_SENDER_ID",
-    appId: "TU_APP_ID",
-    databaseURL: "TU_DATABASE_URL"
+  apiKey: "AIzaSyDcwkUmqaiguV2MoEPsz8HSWUD9uzTIS-w",
+  authDomain: "empaques-multiples-almuerzo.firebaseapp.com",
+  databaseURL: "https://empaques-multiples-almuerzo-default-rtdb.firebaseio.com",
+  projectId: "empaques-multiples-almuerzo",
+  storageBucket: "empaques-multiples-almuerzo.firebasestorage.app",
+  messagingSenderId: "687266639137",
+  appId: "1:687266639137:web:084e8cbe40bb2e001a2260",
+  measurementId: "G-2NRREN25E3"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
-
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Referencias a servicios de Firebase
 const auth = firebase.auth();
 const database = firebase.database();
 
 // Email del administrador
-const ADMIN_EMAIL = "admin@casaempaques.com";
+const ADMIN_EMAIL = "soporte.it.casaempaques@gmail.com";
